92c5f9d9d274117a4fde0d71ec7d15c8d21f0932
msaast <matias.saastamoinen@gmail.com>
2018-11-03 11:50:19 +0200

    log-filename

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 test_cases.py firmware/tamk_1.bin | tee test1_print.txt
-python3 test_cases.py firmware/tamk_2.bin | tee test2_print.txt
-python3 test_cases.py firmware/tamk_3.bin | tee test3_print.txt
-python3 test_cases.py firmware/tamk_4.bin | tee test5_print.txt
+python3 test_cases.py firmware/tamk_1.bin | tee log1.txt
+python3 test_cases.py firmware/tamk_2.bin | tee log2.txt
+python3 test_cases.py firmware/tamk_3.bin | tee log3.txt
+python3 test_cases.py firmware/tamk_4.bin | tee log4.txt
